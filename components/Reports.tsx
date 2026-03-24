c9fc51186ca8a8988ee1673fca6039a918f50607
simaorepresentacoesalimentos-commits <simaorepresentacoesalimentos@gmail.com>
2026-03-24 11:46:45 -0300

    Update Reports.tsx

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -284,8 +284,8 @@ const Reports: React.FC = () => {
 </td>
                     </tr>
                 ))}
-                <tfoot>
-                </tbody>
+               </tbody>
+                </tfoot>
                   <tr className="total-row bg-slate-900 text-white font-black">
                     <td colSpan={2} className="px-6 py-5 text-[10px] uppercase">Totais do Período</td>
                     <td className="px-6 py-5 text-center">{filteredOrders.length} Vendas</td>

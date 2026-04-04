2422a89db5a2bb6924421c0da000495d50c47c5a
simaorepresentacoesalimentos-commits <simaorepresentacoesalimentos@gmail.com>
2026-04-03 22:32:12 -0300

    Update Reports.tsx

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -445,8 +445,8 @@ const Reports: React.FC = () => {
       filters.ramo === 'todos' ||
       (c.ramo || '').toLowerCase().includes((filters.ramo || '').toLowerCase())
     )
-    .sort((a, b) => a.name.localeCompare(b.name))
-       .map((c, index) =>(     
+    {filteredCustomers.sort((a, b) => a.name.localeCompare(b.name))
+      .map((c, index) => (  
                      <tr key={c.id} className="text-[11px] hover:bg-slate-50">
               <td className="px-4 py-3 font-black text-slate-800 uppercase">
                 {index + 1} - {c.name}
